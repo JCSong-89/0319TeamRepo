50d77e8dd7446144a8778a311f48021f08812d92
JCSong-89 <63644722+JCSong-89@users.noreply.github.com>
2025-03-21 11:05:36 +0900

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -42,12 +42,11 @@ def driver():
     user_agent = random.choice(user_agents)
 
     chrome_options = Options() 
-    headless = request.config.getoption("--headless")
 
-    if headless:
-        chrome_options .add_argument("--headless")
-        chrome_options .add_argument("--no-sandbox")
-        chrome_options .add_argument("--disable-dev-shm-usage")
+
+    chrome_options .add_argument("--headless")
+    chrome_options .add_argument("--no-sandbox")
+    chrome_options .add_argument("--disable-dev-shm-usage")
 
     # 1) User-Agent 변경
     chrome_options.add_argument(f'user-agent={user_agent}')
